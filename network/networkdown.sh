2861e1258e62770c9cbcebf8d9d5b576e4b62e9c
blockchain-teacher <150416641+blockchain-teacher@users.noreply.github.com>
2023-11-16 15:19:03 +0900

    Update networkdown.sh

--- network/networkdown.sh
+++ network/networkdown.sh
@@ -1,10 +1,16 @@
 docker-compose -f docker-compose.yaml down -v
 
+docker rmi -f $(docker images dev-* -q)
+
 rm -rf ./config/*
 
 rm -rf ./organizations/peerOrganizations
 rm -rf ./organizations/ordererOrganizations
 
 rm -f ./connection-org1.json
+rm -f ./connection-org2.json
 
-sudo rm -rf ./organizations/fabric-ca/*
+sudo rm -rf ./organizations/fabric-ca/org1/*
+sudo rm -rf ./organizations/fabric-ca/org2/*
+sudo rm -rf ./organizations/fabric-ca/org3/*
+sudo rm -rf ./organizations/fabric-ca/ordererOrg/*
